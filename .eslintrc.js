d4fc6486f5ab31daf76d84a229065571c6602c73
yu7400ki <yu7400ki@gmail.com>
2023-01-11 18:39:40 +0900

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "prefer-arrow",
+    "react-hooks",
+    "import",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "prefer-arrow/prefer-arrow-functions": [
+      "error",
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
