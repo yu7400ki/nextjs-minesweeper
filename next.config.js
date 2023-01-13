7de24ffff5659ccfe8aa4bc6b0627ac5dc769ea8
yu7400ki <yu7400ki@gmail.com>
2023-01-13 17:56:47 +0900

    assetPrefixを追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const assetPrefix =
+  process.env.NODE_ENV === "production" ? "/next-minesweeper" : "";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
